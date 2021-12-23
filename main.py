6347c836afe143c87a13373c0acee78199a684b0
Peter <pdeng.ps@gmail.com>
2021-12-23 16:18:56 +0200

    add error handling

--- main.py
+++ main.py
@@ -1,17 +1,37 @@
 from tkinter import *
 from tkinter import messagebox
-from password_generator import generate_password
+from password_generator import generate_password, decode_password, encode_password
 import pyperclip
+import json
+
+
+def search_password():
+    website = website_entry.get()
+
+    try:
+        with open("password.json", "r") as data_file:
+            data = json.load(data_file)
+    except FileNotFoundError:
+        messagebox.showerror("Error", "No Data File Found")
+    else:
+        password_data = data.get(website, None)
+        if password_data: 
+            messagebox.showinfo("Result", f"Username/Email: {password_data['email']} \nPassword: {decode_password(password_data['password'])}")
+        else:
+            messagebox.showwarning("Warning 404", "No details found for website.")
+
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
+
 def populate_generated_password():
-  password = generate_password()
-  pyperclip.copy(password)
-  password_entry.insert(0, password)
+    password = generate_password()
+    pyperclip.copy(password)
+    password_entry.insert(0, password)
+
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 def entry_validation(entry_text: str, field_name: str) -> bool:
-    if len(entry_text) == 0:
+    if entry_text == '':
         messagebox.showwarning("Warning", f"{field_name} cannot be empty")
         return False
     return True
@@ -21,22 +41,31 @@ def save():
     website = website_entry.get()
     email = email_entry.get()
     password = password_entry.get()
-
+    password_dict = {
+        website: {
+            "email": email,
+            "password": encode_password(password),
+        }
+    }
+    
     if (
         entry_validation(website, "website")
         and entry_validation(email, "email")
         and entry_validation(password, "password")
     ):
-        is_ok = messagebox.askokcancel(
-            title=website,
-            message=f"These are the details entered: \nEmail: {email} \nPassword: {password} \n Is it ok to save?",
-        )
-
-        if is_ok:
-            with open("password.txt", mode="a") as date_file:
-                date_file.write(f"{website} | {email} | {password}\n")
-                website_entry.delete(0, END)
-                password_entry.delete(0, END)
+        try:
+            with open("password.json", mode="r") as data_file:
+                data = json.load(data_file)
+                data.update(password_dict)
+        except FileNotFoundError:
+            with open("password.json", mode="w") as data_file:
+                json.dump(password_dict, data_file, indent=4)
+        else:
+            with open("password.json", mode="w") as data_file:
+                json.dump(data, data_file, indent=4)
+        finally:
+            website_entry.delete(0, END)
+            password_entry.delete(0, END)
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -59,20 +88,24 @@ website_label.grid(row=1, column=0)
 email_username_label.grid(row=2, column=0)
 password_label.grid(row=3, column=0)
 
-website_entry = Entry(width=35)
+website_entry = Entry(width=21)
 email_entry = Entry(width=35)
 password_entry = Entry(width=21)
 
-website_entry.grid(row=1, column=1, columnspan=2)
+website_entry.grid(row=1, column=1)
 email_entry.grid(row=2, column=1, columnspan=2)
 password_entry.grid(row=3, column=1)
 
 website_entry.focus()
 email_entry.insert(0, "pdeng.ps@gmail.com")
-generate_password_button = Button(text="Generate Password", command=populate_generated_password)
+generate_password_button = Button(
+    text="Generate Password", command=populate_generated_password
+)
+search_button = Button(text="Search", width=14, command=search_password)
 add_button = Button(text="Add", width=36, command=save)
 
 generate_password_button.grid(row=3, column=2)
+search_button.grid(row=1, column=2)
 add_button.grid(
     row=4,
     column=1,
--- password_generator.py
+++ password_generator.py
@@ -1,5 +1,7 @@
 
 import random
+import base64
+
 letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
@@ -10,19 +12,19 @@ nr_numbers = random.randint(2, 4)
 
 
 def generate_password():
-  password_list = []
+    password_list = [random.choice(letters) for char in range(nr_letters)]
 
-  password_list.extend([random.choice(letters) for char in range(nr_letters)])
+    password_list.extend([random.choice(numbers) for char in range(nr_symbols)])
 
-  password_list.extend([random.choice(numbers) for char in range(nr_symbols)])
+    password_list.extend([random.choice(symbols) for char in range(nr_numbers)])
 
-  password_list.extend([random.choice(symbols) for char in range(nr_numbers)])
+    random.shuffle(password_list)
 
-  random.shuffle(password_list)
+    return "".join(password_list)
 
-  password = "".join(password_list)
-  print(password)
-  return password
 
-generate_password()
+def encode_password(password:str):
+    return base64.b64encode(bytes(password, 'utf-8')).decode('ascii')
 
+def decode_password(password:str):
+    return base64.b64decode(password).decode()
